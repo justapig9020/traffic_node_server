9a89a37c37c966b9d14410a64be251adee34575f
justapig9020 <justapig9020@gmail.com>
2019-10-20 21:14:25 +0100

    packing...

--- node.c
+++ node.c
@@ -1,6 +1,81 @@
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include "shmctl.h"
+#include "server.h"
+
+static struct shmpg *shm = NULL;
+static int cltFd = -1;
+static struct sockaddr_in serInfo;
 
 int send_d(int n, const struct data d)
 {
+    if (shm == NULL) {
+        fprintf (stderr, "Server doesnt active\n");
+        return -1;
+    }
+    if (cltFd == -1) {
+        cltFd = socket (AF_INET, SOCK_DGRAM, 0);
+        if (cltFd == -1) {
+            perror ("Creat socket in send_d:");
+            return -1;
+        }
 
+        bzero (&serInfo, sizeof(struct sockaddr_in));
+        serInfo.sin_family = AF_INET;
+        serInfo.sin_port = htons (SER_PORT);
+    }
+    if (n == -1) {
+        serInfo.sin_addr.s_addr = htonl(INADDR_ANY);
+    } else {
+        serInfo.sin_addr.s_addr = shm->adj[n].ip;
+    }
+    sendto (cltFd, &d, sizeof(struct data), 0, (struct sockaddr *)&serInfo, sizeof(struct sockaddr));
+    return 0;
+}
+
+struct data recv_d(int n)
+{
+    struct data d;
+    if (shm == NULL) {
+        fprintf (stderr, "Server doesnt active\n");
+        d.rate = -1;
+        d.next = NULL;
+        return d;
+    }
+    d = pop_data (&(shm->adj[n]));
+    return d;
+}
+
+int start_server()
+{
+    shm = exec_server ();
+    if (shm == (struct shmpg *) -1) {
+        return -1;
+    }
+}
+
+int stop_server()
+{
+    struct data d;
+    if (shm == NULL) {
+        fprintf (stderr, "Server doesnt active\n");
+        return -1;
+    }
+    d.rate = -1;
+    d.next = NULL;
+    if (send_d (-1, d) == -1) {
+        fprintf (stderr, "@ stop server\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    return 0;
 }
---
+++ packing.c
@@ -0,0 +1,77 @@
+#include <Python.h>
+
+#include "shmctl.h"
+#include "node.h"
+
+PyMODINIT_FUNC PyInit_node ();
+static struct PyModuleDef traf_node_module;
+static PyMethodDef traf_node_methods[];
+static PyObject *_send_d(PyObject *, PyObject *);
+static PyObject *_recv_d(PyObject *, PyObject *);
+static PyObject *_start_server(PyObject *, PyObject *);
+static PyObject *_stop_server(PyObject *, PyObject *);
+
+PyMODINIT_FUNC PyInit_traf_node() 
+{
+    return PyModule_Create (&traf_node_module);
+}
+
+static struct PyModuleDef traf_node_module = {
+    PyModuleDef_HEAD_INIT, 
+    "traf_node",
+    "c library for claculation",
+    -1,
+    traf_node_methods
+};
+
+static PyMethodDef traf_node_methods[] = {
+    {"send_d", (PyCFunction) _send_d, METH_VARARGS, "Send a data to specific adjacent node by node index"},
+    {"recv_d", (PyCFunction) _recv_d, METH_VARARGS, "Recv a data from specific adjacent node by node index"},
+    {"start_server", (PyCFunction) _start_server, METH_NOARGS, "Start background server"},
+    {"stop_server", (PyCFunction) _stop_server, METH_NOARGS, "Stop background server"},
+    {NULL, NULL, 0, NULL} 
+};
+
+static PyObject *_send_d(PyObject *self, PyObject *args)
+{
+    int n;
+    struct data d;
+    int ret;
+    if (!PyArg_ParseTuple (args, "ii", &n, &(d.rate)))
+        return Py_BuildValue ("i", -1);
+
+    d.next = NULL;
+    ret = send_d (n, d);
+
+    return Py_BuildValue ("i", ret);
+}
+
+static PyObject *_recv_d(PyObject *self, PyObject *args)
+{
+    int n;
+    struct data d;
+    if (!PyArg_ParseTuple (args, "i", &n))
+        return Py_BuildValue ("i", -1);
+
+    d = recv_d (n);
+
+    return Py_BuildValue ("i", d.rate);
+}
+
+static PyObject *_start_server(PyObject *self)
+{
+    int ret;
+
+    ret = start_server ();
+
+    return Py_BuildValue ("i", ret);
+}
+
+static PyObject *_stop_server(PyObject *self)
+{
+    int ret;
+
+    ret = stop_server ();
+
+    return Py_BuildValue ("i", ret);
+}
